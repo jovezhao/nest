52543c09aedb39b2f734b0681f6e542be02b7410
李文科 <liwenke@ypzdw.com>
2016-09-08 22:46:53 +0800

    按类型获取 改为按名字获取

--- nest-ddd/src/main/java/com/ywkj/nest/ddd/RepositoryLoader.java
+++ nest-ddd/src/main/java/com/ywkj/nest/ddd/RepositoryLoader.java
@@ -18,7 +18,8 @@ public class RepositoryLoader<T extends EntityObject> implements IBuilder<T> {
         //先查询缓存中是否存在，如果不正在再从仓储获取
         T t = EntityObjectCacheManager.get(tClass, id);
         if (t == null) {
-            IRepository<T> repository = SpringUtils.getInstance(IRepository.class);
+            String beaniD=tClass.getSimpleName()+"_Repository";
+            IRepository<T> repository = SpringUtils.getInstance(IRepository.class,beaniD);
             t = repository.getEntityById(id);
             EntityObjectCacheManager.put(t);
         }
