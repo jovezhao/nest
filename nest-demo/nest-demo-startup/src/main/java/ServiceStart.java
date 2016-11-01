43dfcf91604d519657899fed80bfa91030182ac1
赵付军 <zhaofujun@ypzdw.com>
2016-11-01 15:33:16 +0800

    测试kafka

--- nest-demo/nest-demo-startup/src/main/java/ServiceStart.java
+++ nest-demo/nest-demo-startup/src/main/java/ServiceStart.java
@@ -3,6 +3,9 @@ import apis.ProductDto;
 import com.ywkj.nest.core.PageList;
 import com.ywkj.nest.core.utils.SpringUtils;
 import com.ywkj.nest.ddd.BeanNotFoundException;
+import com.ywkj.nest.ddd.EventBus;
+import com.ywkj.nest.ddd.IEventHandler;
+import com.ywkj.nest.ddd.ServiceEvent;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -11,38 +14,49 @@ import java.io.IOException;
  * Created by Jove on 2016/9/22.
  */
 public class ServiceStart {
+
+
     public static void main(String[] args) throws IOException, BeanNotFoundException {
+
+
         //加载服务配置并且启动服务
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[]{
                 "classpath*:applicationContext.xml"
         });
         SpringUtils.setApplicationContext(applicationContext);
+        EventBus eventBus = SpringUtils.getInstance(EventBus.class);
+        eventBus.publish(ServiceEvent.createEvent("test", "aaaa"));
+        testHandler handler = new testHandler();
+        eventBus.registerHandler(handler);
 
-        IProductService service = SpringUtils.getInstance(IProductService.class);
-        // region 这里直接模拟客户端调用接口
-        service.addInCart("commodityId");
-        // endregion
 
-        // region 这里模拟查询列表
-        PageList<ProductDto> productDtoPageList = service.getProducts("test", 0, 3);
-        //endregion
-
-//        TestDDD testDDD = new TestDDD();
-//        Thread thread1 = new Thread(testDDD);
-//        thread1.setName("t1");
-//        thread1.start();
-//        Thread thread2 = new Thread(testDDD);
-//        thread2.setName("t2");
-//        thread2.start();
-//        System.in.read();
-//        testDDD.stop();
-
-        ProductDto dto = new ProductDto();
-        dto.setTitle("tttt");
-        dto.setId("commodityId");
-        dto.setPrice(1010);
-
-        service.newProduct(dto);
+//
+//
+//        IProductService service = SpringUtils.getInstance(IProductService.class);
+//        // region 这里直接模拟客户端调用接口
+//        service.addInCart("commodityId");
+//        // endregion
+//
+//        // region 这里模拟查询列表
+//        PageList<ProductDto> productDtoPageList = service.getProducts("test", 0, 3);
+//        //endregion
+//
+////        TestDDD testDDD = new TestDDD();
+////        Thread thread1 = new Thread(testDDD);
+////        thread1.setName("t1");
+////        thread1.start();
+////        Thread thread2 = new Thread(testDDD);
+////        thread2.setName("t2");
+////        thread2.start();
+////        System.in.read();
+////        testDDD.stop();
+//
+//        ProductDto dto = new ProductDto();
+//        dto.setTitle("tttt");
+//        dto.setId("commodityId");
+//        dto.setPrice(1010);
+//
+//        service.newProduct(dto);
     }
 }
 
@@ -66,3 +80,15 @@ public class ServiceStart {
 //        }
 //    }
 //}
+class testHandler implements IEventHandler<String> {
+
+    @Override
+    public String getEventName() {
+        return "test";
+    }
+
+    @Override
+    public void handle(String data) {
+
+    }
+}
