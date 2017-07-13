ba4064ea6c89d4e773c96eb5213c96316fd7939f
zhaofujun <jovezhao@foxmail.com>
2017-07-13 14:01:05 +0800

    发布包到中央仓库

--- nest-starter-test/src/main/java/com/jovezhao/nest/test/Application.java
+++ nest-starter-test/src/main/java/com/jovezhao/nest/test/Application.java
@@ -1,19 +1,11 @@
 package com.jovezhao.nest.test;
 
-import com.jovezhao.nest.activemq.ActiveMQChannelProvider;
-import com.jovezhao.nest.activemq.ActiveMQProviderConfig;
-import com.jovezhao.nest.ddd.event.*;
-import com.jovezhao.nest.exception.CustomException;
-import com.jovezhao.nest.log.Log;
-import com.jovezhao.nest.log.LogAdapter;
-import com.jovezhao.nest.test.api.TestDto;
-import com.jovezhao.nest.test.api.UserService;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Created by zhaofujun on 2017/6/17.
@@ -21,55 +13,61 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.jovezhao.nest.test.repositories.mappers")
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer {
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
-
-    @Autowired
-    private UserService userService;
-
     @Override
-    public void run(String[] args) throws Exception {
-        ChannelProvider channelProvider = new ActiveMQChannelProvider();
-
-        ActiveMQProviderConfig providerConfig = new ActiveMQProviderConfig();
-        providerConfig.setBrokers("tcp://127.0.0.1:61616");
-        channelProvider.setProviderConfig(providerConfig);
-        channelProvider.init();
-
-        EventConfigItem eventConfigItem = new EventConfigItem();
-        eventConfigItem.setEventName("event1");
-        eventConfigItem.setChannelProvider(channelProvider);
-        EventConfigManager.put(eventConfigItem);
-
-
-        EventBus.registerHandler(new TestHandler());
-        EventBus.registerHandler(new TestHandler());
-
-
-        userService.changeName("new 55");
-
-
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
-
-    public class TestHandler implements EventHandler<TestDto> {
-        private Log log = new LogAdapter(this.getClass());
-
-        @Override
-        public String getEventName() {
-            return "event1";
-        }
-
-        @Override
-        public Class<TestDto> getTClass() {
-            return TestDto.class;
-        }
-
-        @Override
-        public void handle(TestDto data) throws Exception {
-            log.info(data.getAbs());
-            //System.out.println("fffffff::" + data.getAbs());
-        }
-    }
+//    @Autowired
+//    private UserService userService;
+//
+//    @Override
+//    public void run(String[] args) throws Exception {
+////        ChannelProvider channelProvider = new ActiveMQChannelProvider();
+////
+////        ActiveMQProviderConfig providerConfig = new ActiveMQProviderConfig();
+////        providerConfig.setBrokers("tcp://127.0.0.1:61616");
+////        channelProvider.setProviderConfig(providerConfig);
+////        channelProvider.init();
+////
+////        EventConfigItem eventConfigItem = new EventConfigItem();
+////        eventConfigItem.setEventName("event1");
+////        eventConfigItem.setChannelProvider(channelProvider);
+////        EventConfigManager.put(eventConfigItem);
+////
+////
+////        EventBus.registerHandler(new TestHandler());
+////        EventBus.registerHandler(new TestHandler());
+////
+////
+////        userService.changeName("new 55");
+////
+//
+//    }
+//
+//    public class TestHandler implements EventHandler<TestDto> {
+//        private Log log = new LogAdapter(this.getClass());
+//
+//        @Override
+//        public String getEventName() {
+//            return "event1";
+//        }
+//
+//        @Override
+//        public Class<TestDto> getTClass() {
+//            return TestDto.class;
+//        }
+//
+//        @Override
+//        public void handle(TestDto data) throws Exception {
+//            log.info(data.getAbs());
+//            //System.out.println("fffffff::" + data.getAbs());
+//        }
+//    }
 }
