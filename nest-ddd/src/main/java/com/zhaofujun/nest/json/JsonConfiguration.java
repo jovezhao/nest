bd39a3f09b46d92957f2e3f17f4e74601cf0a4e3
TESLA_CN <root496879118@me.com>
2019-12-26 17:46:14 +0800

    :sparkles: 添加Gson配置项

--- nest-ddd/src/main/java/com/zhaofujun/nest/json/JsonConfiguration.java
+++ nest-ddd/src/main/java/com/zhaofujun/nest/json/JsonConfiguration.java
@@ -1,13 +1,38 @@
 package com.zhaofujun.nest.json;
 
+import java.util.Objects;
+
 public class JsonConfiguration {
     private Class clazz;
+    private Object typeAdapter;
 
     public JsonConfiguration(Class clazz) {
         this.clazz = clazz;
     }
 
+    public JsonConfiguration(Class clazz, Object typeAdapter) {
+        this.clazz = clazz;
+        this.typeAdapter = typeAdapter;
+    }
+
     public Class getClazz() {
         return clazz;
+    }
+
+    public Object getTypeAdapter() {
+        return typeAdapter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JsonConfiguration that = (JsonConfiguration) o;
+        return Objects.equals(clazz, that.clazz);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clazz);
     }
 }
