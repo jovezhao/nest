e26f3956caa723c63a17435cabab6a0f36fdf8da
jovezhao <jovezhao@foxmail.com>
2020-08-12 17:07:42 +0800

    添加日期序列化为时间戳

---
+++ nest-ddd/src/main/java/com/zhaofujun/nest/json/DateAdapter.java
@@ -0,0 +1,20 @@
+package com.zhaofujun.nest.json;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+
+public class DateAdapter implements JsonSerializer<Date>, JsonDeserializer<Date> {
+
+    @Override
+    public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(src.getTime());    }
+
+    @Override
+    public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
+        Long timestamp = json.getAsJsonPrimitive().getAsLong();
+        if (0L == timestamp) return null;
+        return new Date(timestamp);
+    }
+}
