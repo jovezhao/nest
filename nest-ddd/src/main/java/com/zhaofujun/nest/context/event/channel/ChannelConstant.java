1ddd0b8a5fdae22237792e71c4c97947004f0f62
严祥 <yanxinag@kupanda.cn>
2020-08-13 15:42:58 +0800

    1.新增MQ通道常量,可用作通道筛选器进行后续开发

---
+++ nest-ddd/src/main/java/com/zhaofujun/nest/context/event/channel/ChannelConstant.java
@@ -0,0 +1,15 @@
+package com.zhaofujun.nest.context.event.channel;
+
+/**
+ * 消息通道常量
+ */
+public class ChannelConstant {
+
+    public static final String ACTIVE_MQ_CHANNEL_CODE="ActiveMQMessageChannel";
+
+    public static final String RABBIT_MQ_CHANNEL_CODE="RabbitMQMessageChannel";
+
+    public static final String ROCKET_MQ_CHANNEL_CODE="RocketMQMessageChannel";
+
+    private static final String ALIYUN_ROCKET_MQ_CHANNEL_CODE="AliYunRocketMQMessageChannel";
+}
