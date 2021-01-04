c520bba97937e7aee4943256ada77885d17e125d
jovezhao <jovezhao@foxmail.com>
2021-01-04 15:41:58 +0800

    更新错误码

--- nest-ddd/src/main/java/com/zhaofujun/nest/exception/ExceptionCode.java
+++ nest-ddd/src/main/java/com/zhaofujun/nest/exception/ExceptionCode.java
@@ -4,6 +4,6 @@ public class ExceptionCode {
     public static final int VerifyFailed = 10001;
     public static final int EntityExisted = 10002;
     public static  final  int VersionConflicted=10003;
-    public static final int Null = 10003;
+    public static final int Null = 10004;
 
 }
