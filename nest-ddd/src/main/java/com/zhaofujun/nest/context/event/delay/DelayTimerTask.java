bbeec66c503bb332f189f7ff1633d28224522515
jovezhao <jovezhao@foxmail.com>
2021-03-11 15:54:32 +0800

    处理重发消息和延迟消息异常问题

--- nest-ddd/src/main/java/com/zhaofujun/nest/context/event/delay/DelayTimerTask.java
+++ nest-ddd/src/main/java/com/zhaofujun/nest/context/event/delay/DelayTimerTask.java
@@ -6,7 +6,9 @@ import com.zhaofujun.nest.context.event.message.MessageBacklog;
 import com.zhaofujun.nest.context.event.message.MessageInfo;
 import com.zhaofujun.nest.json.JsonCreator;
 import com.zhaofujun.nest.json.ParameterizedTypeFactory;
+import com.zhaofujun.nest.standard.CustomException;
 import com.zhaofujun.nest.standard.EventData;
+import com.zhaofujun.nest.standard.SystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,26 +20,34 @@ public class DelayTimerTask extends TimerTask {
 
     @Override
     public void run() {
-        DelayMessageStore delayMessageStore = DelayMessageStoreFactory.create();
-        List<MessageBacklog> messageBacklogs = delayMessageStore.getAndLock();
-        messageBacklogs.forEach(messageBacklog -> {
-            if (messageBacklog != null) {
-                logger.debug("投递延迟消息" + messageBacklog.getEventCode());
-                Class eventDataType = null;
-                try {
-                    eventDataType = Class.forName(messageBacklog.getEventDataType());
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    return;
-                }
+        try {
+            DelayMessageStore delayMessageStore = DelayMessageStoreFactory.create();
+            List<MessageBacklog> messageBacklogs = delayMessageStore.getAndLock();
+            messageBacklogs.forEach(messageBacklog -> {
+                if (messageBacklog != null) {
+                    logger.debug("投递延迟消息" + messageBacklog.getEventCode());
+                    Class eventDataType = null;
+                    try {
+                        eventDataType = Class.forName(messageBacklog.getEventDataType());
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                        return;
+                    }
 
-                MessageInfo messageInfo = JsonCreator.getInstance().toObj(messageBacklog.getMessageInfoString(), ParameterizedTypeFactory.make(MessageInfo.class, eventDataType));
-                EventData eventData = messageInfo.getData();
-                ResendAppService appService = ApplicationServiceCreator.create(ResendAppService.class);
-                appService.send(eventData);
-                delayMessageStore.clear(messageInfo.getMessageId());
-            }
-        });
+                    MessageInfo messageInfo = JsonCreator.getInstance().toObj(messageBacklog.getMessageInfoString(), ParameterizedTypeFactory.make(MessageInfo.class, eventDataType));
+                    EventData eventData = messageInfo.getData();
+                    ResendAppService appService = ApplicationServiceCreator.create(ResendAppService.class);
+                    appService.send(eventData);
+                    delayMessageStore.clear(messageInfo.getMessageId());
+                }
+            });
+        } catch (CustomException ex) {
+            logger.warn("发生业务异常", ex);
+        } catch (SystemException ex) {
+            logger.error("发送系统异常", ex);
+        } catch (Exception ex) {
+            logger.error("发送未知异常", ex);
+        }
     }
 }
 
