37373c8b0fbe926f91b4b88f84b47bb5497f4660
jovezhao <jovezhao@foxmail.com>
2021-05-21 10:48:50 +0800

    修复延迟消息存储代号获取错误的问题

--- nest-ddd/src/main/java/com/zhaofujun/nest/context/event/delay/DelayMessageStoreFactory.java
+++ nest-ddd/src/main/java/com/zhaofujun/nest/context/event/delay/DelayMessageStoreFactory.java
@@ -1,18 +1,17 @@
 package com.zhaofujun.nest.context.event.delay;
 
 import com.zhaofujun.nest.NestApplication;
-import com.zhaofujun.nest.context.event.resend.MessageResendStore;
 
 public class DelayMessageStoreFactory {
 
     public static DelayMessageStore create() {
-        return create(NestApplication.current().getMessageConfiguration().getResendStore());
+        return create(NestApplication.current().getMessageConfiguration().getDelayStore());
     }
 
     public static DelayMessageStore create(String code) {
-        DelayMessageStore messageResendStore = NestApplication.current().getProviderManage().getDelayMessageStore(code);
-        if (messageResendStore == null)
-            return create(NestApplication.current().getMessageConfiguration().getResendStore());
-        return messageResendStore;
+        DelayMessageStore messageDelayStore = NestApplication.current().getProviderManage().getDelayMessageStore(code);
+        if (messageDelayStore == null)
+            return create(NestApplication.current().getMessageConfiguration().getDelayStore());
+        return messageDelayStore;
     }
 }
