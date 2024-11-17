a5bd34b25f09e1a7e0ee52beb1086f44029d8965
jovezhao <jovezhao@foxmail.com>
2024-11-17 17:03:35 +0800

    为 demo 添加事件发布

--- nest-demo/nest-demo-app/src/main/java/com/zhaofujun/nest/demo/appservices/UserAppservice.java
+++ nest-demo/nest-demo-app/src/main/java/com/zhaofujun/nest/demo/appservices/UserAppservice.java
@@ -5,6 +5,7 @@ import com.zhaofujun.nest.ddd.LongIdentifier;
 import com.zhaofujun.nest.demo.appservices.model.User;
 import com.zhaofujun.nest.demo.webapi.dto.UserDto;
 import com.zhaofujun.nest.utils.EntityUtil;
+import com.zhaofujun.nest.utils.EventUtil;
 
 @AppService
 public class UserAppservice {
@@ -15,6 +16,7 @@ public class UserAppservice {
         var result = new UserDto();
         result.setId(user.getId().toValue());
         result.setName(user.getName());
+        EventUtil.publish("test", result);
         return result;
     }
 }
